3f429353823f2b1c49272772aeb5ae96f14ccc49
n0mori <nick33omori@gmail.com>
2020-01-22 07:54:48 -0300

    new metrics

--- svm.py
+++ svm.py
@@ -4,6 +4,8 @@ from encode import create_vectors
 from encode import train as encode
 from sklearn.svm import OneClassSVM
 from sklearn.svm import SVC
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 from reader import get_traces
 import numpy as np
 import os
@@ -11,7 +13,7 @@ import re
 from random import shuffle
 
 
-def one_class(log_name, test_vectors, train, test, normal_traces, nu):
+def one_class(log_name, test_vectors, train, test, normal_traces, nu, labels):
     ocsvm = OneClassSVM(kernel='linear', nu=nu)
     ocsvm.fit(train)
     classes = list(ocsvm.predict(test))
@@ -22,17 +24,20 @@ def one_class(log_name, test_vectors, train, test, normal_traces, nu):
     anom_checked = [is_normal(t, normal_traces) for t in anom_cases]
     normal_checked = [is_normal(t, normal_traces) for t in normal_cases]
 
-    acc = (anom_checked.count(False) + normal_checked.count(True)) / float(len(test)) 
+    f1 = f1_score(labels, classes)
+    auc = roc_auc_score(labels, classes)
+    acc = accuracy_score(labels, classes)
+    # acc = (anom_checked.count(False) + normal_checked.count(True)) / float(len(test)) 
 
     print(log_name, 
-        "oc", 
+        "OCSVM", 
         nu, 
-        acc, sep=",")
+        f1, auc, acc, sep=",")
 
 
-def supervised(log_name, test_vectors, train, test, normal_traces, labels):
+def supervised(log_name, test_vectors, train, test, normal_traces, train_labels, test_labels):
     svc = SVC(kernel='linear')
-    svc.fit(train, labels)
+    svc.fit(train, train_labels)
     classes = list(svc.predict(test))
 
     anom_cases = [test_vectors[k] for k, x in enumerate(classes) if x == -1]
@@ -41,12 +46,34 @@ def supervised(log_name, test_vectors, train, test, normal_traces, labels):
     anom_checked = [is_normal(t, normal_traces) for t in anom_cases]
     normal_checked = [is_normal(t, normal_traces) for t in normal_cases]
 
-    acc = (anom_checked.count(False) + normal_checked.count(True)) / float(len(test))
+    f1 = f1_score(test_labels, classes)
+    auc = roc_auc_score(test_labels, classes)
+    acc = accuracy_score(test_labels, classes)
+    # acc = (anom_checked.count(False) + normal_checked.count(True)) / float(len(test))
 
     print(log_name, 
-          "sv", 
+          "SVM", 
           "",
-          acc, sep=",")
+          f1, auc, acc, sep=",")
+
+
+def lof(log_name, test_vectors, train, test, normal_traces, k=20, contamination=0.01):
+    model = LocalOutlierFactor(k, contamination=contamination, novelty=True)
+    model.fit(train)
+    classes = list(model.predict(test))
+
+    anom_cases = [test_vectors[k] for k, x in enumerate(classes) if x == -1]
+    normal_cases = [test_vectors[k] for k, x in enumerate(classes) if x == 1]
+
+    anom_checked = [is_normal(t, normal_traces) for t in anom_cases]
+    normal_checked = [is_normal(t, normal_traces) for t in normal_cases]
+
+    acc = (anom_checked.count(False) + normal_checked.count(True)) / float(len(test)) 
+
+    print(log_name, 
+        "lof", 
+        k, 
+        acc, sep=",")
 
 
 def train(log_name, traces_name, w2v_model):
@@ -62,12 +89,15 @@ def train(log_name, traces_name, w2v_model):
     
     # print("log_name,nu,method,accuracy")
 
-    labels = np.array([1 if is_normal(trace, normal_traces) else -1 for trace in traces[:limit]])
+    train_labels = np.array([1 if is_normal(trace, normal_traces) else -1 for trace in traces[:limit]])
+    test_labels = np.array([1 if is_normal(trace, normal_traces) else -1 for trace in traces[limit:]])
 
-    for nu in [0.05, 0.1, 0.3, 0.5]: 
-        one_class(log_name, test_traces, train, test, normal_traces, nu)
+    for nu in [0.001, 0.005, 0.01, 0.05, 0.1, 0.3, 0.5]: 
+        one_class(log_name, test_traces, train, test, normal_traces, nu, test_labels)
+    
+    # lof(log_name, test_traces, train, test, normal_traces)
 
-    supervised(log_name, test_traces, train, test, normal_traces, labels)
+    supervised(log_name, test_traces, train, test, normal_traces, train_labels, test_labels)
 
 
 def is_normal(vector, normal_traces):
